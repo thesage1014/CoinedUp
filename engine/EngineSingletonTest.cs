ddee593033fc512b5f129bdc3cdd8fca61d1adac
Nick Thinkpad X1 <hwerzog@gmail.com>
2023-04-21 17:58:57 -0700

    c# impl of csv parser -> arcade game objects

--- engine/EngineSingletonTest.cs
+++ engine/EngineSingletonTest.cs
@@ -1,16 +1,14 @@
 using Godot;
-using System;
 
 public partial class EngineSingletonTest : Node {
+  public static bool DebugPrinting = true;
   // Called when the node enters the scene tree for the first time.
-  public static void Main(string[] args) {
-    Console.WriteLine ("Hello, World");
-  }
-
   public override void _Ready() {
     GD.Print("Ran test call!");
     GD.Print("Dependency says:");
-    GD.Print(EngineDependency.GetValue());
+    ArcGamesList.LoadTemplateGames();
+    GD.Print(ArcGamesList.Games[0].ArcGameSummary());
+    GD.Print(ArcGamesList.Games[1].ArcGameSummary());
   }
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
