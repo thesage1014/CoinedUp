fde1e61c976de2682cb16b79b583767d384ea3bc
Servald <thesage1014@gmail.com>
2023-05-11 12:04:30 -0700

    beehave MVP

--- scenes/NPC.gd
+++ scenes/NPC.gd
@@ -3,23 +3,25 @@ class_name NPC extends CharacterBody3D
 @onready var navAgent = $NavigationAgent3D
 @onready var animTree = $Body/AnimationTree
 @onready var animator = $Body/AnimationPlayer
+var moveTowardsTarget := false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animator.play("walk and idle/Walking")
 	$Blackboard.set_value("NPC",self)	
+	animTree.set("parameters/TimeScale/scale",2.6)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var pos = global_transform.origin
-	var targetPos = navAgent.get_next_path_position()
-	var facing_angle = Math.zx(targetPos-pos).angle()
-	var newRotation = lerp_angle(global_rotation.y,facing_angle,.2)
-	global_rotation = newRotation * Vector3.UP
-	var movement = Vector3.ZERO
-	if(navAgent.distance_to_target()>.2):
-		movement = (targetPos - pos).normalized()*walkSpeed
-	velocity = movement#velocity.move_toward(movement,.3)
-	animTree.set("parameters/TimeScale/scale",2.6)
+	velocity += ProjectSettings.get_setting("physics/3d/default_gravity_vector")
+	if(moveTowardsTarget):
+		var pos = global_transform.origin
+		var targetPos = navAgent.get_next_path_position()
+		var facing_angle = Math.zx(targetPos-pos).angle()
+		var newRotation = lerp_angle(global_rotation.y,facing_angle,.2)
+		if(navAgent.distance_to_target()>.1):
+			global_rotation = newRotation * Vector3.UP
+			velocity = (targetPos - pos).normalized()*walkSpeed
 	move_and_slide()
 
 
@@ -27,6 +29,7 @@ func set_destination(target):
 	navAgent.target_position = target
 
 func attract_available_square(cabinet : Cabinet):
-	if !navAgent.is_target_reached():
-		navAgent.target_position = cabinet.body.global_position
-		print("attracted to" + str(cabinet))
+	pass
+#	if !navAgent.is_target_reached():
+#		navAgent.target_position = cabinet.body.global_position
+#		print("attracted to" + str(cabinet))
--- scripts/ai/MoveForward.gd
+++ scripts/ai/MoveForward.gd
@@ -2,7 +2,12 @@ extends ActionLeaf
 
 
 func tick(actor: Node, blackboard: Blackboard):
-	NPC npc = blackboard.get_value("NPC")
-	print npc
-	return SUCCESS
+	var npc:NPC = blackboard.get_value("NPC")
+	if(npc.get_node("InteractionArea").has_overlapping_bodies()):
+		return FAILURE
+	else:
+		print("Moving Forward")
+		npc.moveTowardsTarget = false
+		npc.velocity = Vector3.BACK.rotated(Vector3.UP,npc.global_rotation.y)
+		return SUCCESS
 
